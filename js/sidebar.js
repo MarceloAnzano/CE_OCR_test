6bd2d82cc59d6c341a42db29eac83147b319633c
Marcelo Anzano <public.mranzano@gmail.com>
2016-01-27 07:33:17 +0800

    colors, fonts at spacing nalang

--- js/sidebar.js
+++ js/sidebar.js
@@ -1,13 +1,27 @@
 $(document).ready(function() {
+    
+    $("#sidebar-subMenu").css("width",$("#sidebar-menu").outerWidth()+"px");
+    // $("#sidebar-item").css("width",$("#sidebar-menu").outerWidth()+"px");
+    $(".accordion-section-content").css("width",$("#sidebar-menu").outerWidth()+"px");
+    $(".accordion-section-title").css("width",$("#sidebar-menu").outerWidth()+"px");
+
     function close_accordion_section(e) {
-      console.log(e);
         $('.accordion .accordion-section-title').removeClass('active');
         $('.accordion .accordion-section-content').slideUp(300).removeClass('open');
     }
+
+    $('[id^=sidebar-item]').click(function(e){
+      if($(this).find('.arrow-left').css("display") == 'none')
+      {
+        $('[id^=sidebar-item]').find('.arrow-left').css("display","none");
+        $(this).find('.arrow-left').css("display","block");
+      }
+    });
  
     $('.accordion-section-title').click(function(e) {
         // Grab current anchor value
         var currentAttrValue = $(this).attr('href');
+        console.log(e.target)    
  
         if($(e.target).is('.active')) {
             close_accordion_section(e.target);
@@ -22,4 +36,33 @@ $(document).ready(function() {
  
         e.preventDefault();
     });
+
+    $("#sidebar-wrapper").hover(function(e){
+      if($("#sidebar-wrapper").hasClass("collapsed")){
+        $("#sidebar-wrapper").removeClass("collapsed");
+        $(".accordion-section").css("display","block");
+        // $("[class^=accordion-]").css("display","block");
+      }
+      else{
+        $("#sidebar-wrapper").addClass("collapsed");
+        $(".accordion-section").css("display","none");
+        // $("[class^=accordion-]").css("display","none");
+        // $("#sidebar-wrapper").attr("id","sidebar-wrapper-collapsed");
+      }
+    });
+
+    $("#sidebar-wrapper").addClass("collapsed");
+    $("#sidebar-wrapper").css("opacity",1);
+    $(".accordion-section").css("display","none");
+    if($("#sidebar-wrapper").css("opacity") == 1){
+        $("#sidebar-wrapper").css("transition","all 0.5s ease");
+        $("#sidebar-wrapper.collapsed").css("transition","all 0.5s ease");
+    }
+});
+
+$(window).on('resize', function(){
+  
+  $("#sidebar-subMenu").css("width",$("#sidebar-menu").outerWidth()+"px");
+  $(".accordion-section-title").css("width",$("#sidebar-menu").outerWidth()+"px");
+  $(".accordion-section-content").css("width",$("#sidebar-menu").outerWidth()+"px");
 });
